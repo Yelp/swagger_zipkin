ca6396c6916d9eb2856d6b262948f36783c092cd
Anthony Sottile <asottile@umich.edu>
2016-09-26 14:17:21 -0700

    Update python classifiers

--- setup.py
+++ setup.py
@@ -29,9 +29,10 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
     ],
 )
